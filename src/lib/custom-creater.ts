f47b4addd1772aec8c662cc12e562d290b0ed91a
iwe7 <admin@meepo.com.cn>
2018-07-09 19:47:55 +0800

    u

--- src/lib/custom-creater.ts
+++ src/lib/custom-creater.ts
@@ -1,10 +1,12 @@
+import { AppActionService, AddInputAction } from 'iwe7-app';
+import { Store } from '@ngrx/store';
 import { Iwe7UiService } from './iwe7-ui.service';
 import { Directive, Input, ElementRef } from '@angular/core';
 import { ComponentModel } from 'iwe7-app';
 @Directive({ selector: '[customCreater]' })
 export class CustomCreaterDirective {
     @Input()
-    set customCreater(val: ComponentModel[]) {
+    set componentCreater(val: ComponentModel[]) {
         if (val) {
             const newVal = val.sort((a, b) => parseInt(a.component_displayorder, 10) - parseInt(b.component_displayorder, 10));
             const element: HTMLElement = this.ele.nativeElement;
@@ -12,10 +14,19 @@ export class CustomCreaterDirective {
             for (const key in newVal) {
                 const item = newVal[key];
                 this.ui.load(newVal[key].component_selector).pipe().subscribe(ele => {
-                    // 绑定输入
-
-                    // 绑定输出
-
+                    // inputs
+                    ele.id = item.component_id;
+                    this.store.dispatch(new AddInputAction({
+                        id: item.component_id,
+                        ...item.component_inputs
+                    }));
+                    // outputs
+                    for (const key in item.component_outputs) {
+                        ele.addEventListener(key, (e: any) => {
+                            const action = item.component_outputs[key];
+                            this.action.get(action, e.detail);
+                        });
+                    }
                     container.appendChild(ele);
                 });
             }
@@ -23,7 +34,9 @@ export class CustomCreaterDirective {
     }
     constructor(
         public ele: ElementRef,
-        public ui: Iwe7UiService
+        public ui: Iwe7UiService,
+        public action: AppActionService,
+        public store: Store<any>
     ) { }
 
 }
